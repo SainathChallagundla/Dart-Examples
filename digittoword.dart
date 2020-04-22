c7fe69268deb1bdc8d05519df2931548f1f834b2
sai <nathsai1027@gmail.com>
2020-04-22 12:38:43 +0530

    digittoword.dart

---
+++ digittoword.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  stdout.writeln("Enter The Number");
+  int number = int.parse(stdin.readLineSync());
+if(10<=number){
+stdout.writeln("You have Entered Number is Double Digit");
+}
+else if(number<0)
+stdout.writeln("You have Entered Negative Number");
+else if(number==0)
+stdout.writeln("Zero");
+  else if(number == 1) 
+    stdout.writeln("One");
+  else if (number == 2)
+    stdout.writeln("Two");
+  else if (number == 3)
+    stdout.writeln("Three");
+  else if (number == 4)
+    stdout.writeln("Four");
+  else if (number == 5)
+    stdout.writeln("Five");
+  else if (number == 6)
+    stdout.writeln("Six");
+  else if (number == 7)
+    stdout.writeln("Seven");
+  else if (number == 8)
+    stdout.writeln("Eight");
+  else if (number == 9) stdout.writeln("nine");
+}
