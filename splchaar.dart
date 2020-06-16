8f6f7b7024bfc260f6430040f6cf9340698f5c05
sai <nathsai1027@gmail.com>
2020-06-16 23:42:13 +0530

    modified program

---
+++ splchaar.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main(){
+  stdout.writeln("Enter a Matter");
+  String sub=stdin.readLineSync();
+  String spl=stdin.readLineSync();
+
+
+  int i=0;
+
+  while(i<sub.length)
+  if(sub[i]=="~")
+  
+
+
+}
